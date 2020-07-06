76e85687ed02f152e7de071a7eb4283697c43576
unknown <othy.tenk@gmail.com>
2020-07-06 15:22:26 +0800

    Site updated

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -90,18 +90,18 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      Бүх бүтээгдэхүүн
                     </Link>
                   </div>
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Шинэ мэдээлэл
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
-                      Read more
+                      Цааш нь
                     </Link>
                   </div>
                 </div>
